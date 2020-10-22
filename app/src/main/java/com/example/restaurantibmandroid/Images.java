79274de008057c3f1c9dfa6dc5a6c0269ecc1560
odrag <o.dragu10@yahoo.com>
2020-10-22 15:49:10 +0300

    Second commit

---
+++ app/src/main/java/com/example/restaurantibmandroid/Images.java
@@ -0,0 +1,22 @@
+package com.example.restaurantibmandroid;
+
+public class Images {
+
+    private String imagePath;
+
+    public Images() {
+
+    }
+
+    public Images(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
---
+++ app/src/main/java/com/example/restaurantibmandroid/ItemViewHolder.java
@@ -0,0 +1,7 @@
+package com.example.restaurantibmandroid;
+
+import android.view.View;
+
+public interface ItemViewHolder {
+    void onClick(View view, int adapterPosition);
+}
